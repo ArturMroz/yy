7a9c29204a6600a812a72d2c134b09d711f96056
Artur Mroz <frost47@gmail.com>
2023-02-27 21:07:54 +0000

    add new sample

--- web/script.js
+++ web/script.js
@@ -32,27 +32,25 @@ function setupSampleSelect() {
         sampleSelect.appendChild(option)
     }
 
-    sampleSelect.addEventListener('change', e => {
-        const sampleName = e.target.value
-        setSample(sampleName)
-    })
+    sampleSelect.addEventListener('change', e => setSample(e.target.value))
 }
 
 const samples = {
     'hello': {
         content:
-            `// you can edit this code, or select a sample from the dropdown on the right
+            `// You can edit this code, or select a sample from the dropdown on the right.
 // 
-// to run the code click 'Run' button
+// To run the code click 'Run' button.
 
-name := "Yan"\nyap("Hello, {name}!")` },
+name := "Yennefer"
+yap("Hello, {name}!")` },
 
     'fibonacci': {
         content:
             `// recursion
 fib1 := \\n {
   yif n < 2 { n } yels { fib1(n-1) + fib1(n-2) }
-};
+}
 
 yap("seventh Fibonacci number:", fib1(7))
 
@@ -71,6 +69,55 @@ fib2 := \\{
 f := fib2()
 yap("consecutive Fibonacci numbers:", f(), f(), f(), f(), f())
 ` },
+
+    'map et al': {
+        content:
+            `// Map, filter, and reduce are like the three musketeers of functional programming, 
+// banding together to process and transform collections with finesse and style.
+
+// Map transforms all elements and returns a shiny new list.
+map := \\arr fn {
+    acc := []
+    yall arr {
+        acc = push(acc, fn(yt))
+    }
+}
+
+arr    := [1, 2, 3, 4]
+double := \\x { x * 2 }
+yap("doubled:", map(arr, double))
+
+// Reduce takes a list and violently smashes it into a single value.
+reduce := \\arr initial f {
+    result := initial
+    yall arr {
+        result = f(result, yt)
+    }
+}
+
+sum := \\arr {
+    reduce(arr, 0, \\initial el { initial + el })
+}
+
+yap("sum:", sum([1, 2, 3, 4]))
+
+// Filter picks the juiciest elements, leaving all the rest to wither away into obscurity.
+filter := \\arr fn {
+    acc := []
+    yall arr {
+        yif fn(yt) {
+            acc = push(acc, yt)
+        }
+    }
+    acc
+}
+
+arr  := [1, 2, 3, 4, 5, 6, 7]
+avg  := sum(arr) / len(arr)
+smol := \\x { x < avg }
+yap("smaller than average:", filter(arr, smol)) 
+`
+    }
 }
 
 setupSampleSelect()
